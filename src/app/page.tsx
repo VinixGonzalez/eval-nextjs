0d9adee19502270d9f1aec850e0a87602bcc5f02
Vinicius Gonzalez <vinix.fatos@hotmail.com>
2023-05-16 20:17:56 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/button/Button";
+import { Button as ChakraBtn } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-black text-5xl">Eval</h1>
+    </main>
+  );
+}

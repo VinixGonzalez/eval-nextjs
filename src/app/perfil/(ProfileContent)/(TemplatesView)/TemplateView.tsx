6e44f4b252241d4399c9db8a23fb3bb43c1adb78
Vinicius Gonzalez <vinix.fatos@hotmail.com>
2023-06-28 06:52:04 -0300

    update

--- src/app/perfil/(ProfileContent)/(TemplatesView)/TemplateView.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const TemplateView: React.FC = () => {
-  return (
-    <div>
-      <h1>Template View</h1>
-    </div>
-  );
-};
--- src/axios/config.ts
+++
@@ -1,4 +0,0 @@
-import Axios from "axios";
-import { setupCache } from "axios-cache-interceptor";
-
-export const cachedProfileResponse = setupCache(Axios);

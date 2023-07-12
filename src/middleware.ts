f2d735a890d86eceafa8c579756eaa9b8c6df7d6
Vinicius Gonzalez <vinix.fatos@hotmail.com>
2023-07-12 16:11:16 -0300

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,18 @@ export { default } from "next-auth/middleware";
 // middleware.js
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-export function middleware(request: NextRequest) {
-  // Get the admin cookie id from the request.
-  const authorizationCookie = request.cookies.get(
-    "next-auth.session-token"
-  )?.value;
-  // const allCookies = request.cookies.getAll();
+// export function middleware(request: NextRequest) {
+//   // Get the admin cookie id from the request.
+//   const authorizationCookie = request.cookies.get(
+//     "next-auth.session-token"
+//   )?.value;
+//   // const allCookies = request.cookies.getAll();
 
-  // If the auth cookie id is not "abcdefg", redirect the user to the admin login page.
-  if (!authorizationCookie) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-}
+//   // If the auth cookie id is not "abcdefg", redirect the user to the admin login page.
+//   if (!authorizationCookie) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
+// }
 
 export const config = {
   matcher: [

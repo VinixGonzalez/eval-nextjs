ce9376638ff4bf652d756a9be2148eb8ef409335
Vinicius Gonzalez <vinix.fatos@hotmail.com>
2023-07-21 13:34:26 -0300

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,35 +6,35 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import jwtoken from "jsonwebtoken";
 
-export async function middleware(request: NextRequest) {
-  debugger;
-  const tokenEncapsulated: any = await getToken({
-    req: request,
-    secret: process.env.SECRET,
-  });
+// export async function middleware(request: NextRequest) {
+//   debugger;
+//   const tokenEncapsulated: any = await getToken({
+//     req: request,
+//     secret: process.env.SECRET,
+//   });
 
-  const tokenFromJwt: any = jwtoken.decode(
-    tokenEncapsulated?.accessToken.split(" ")[1]
-  );
-  const tokenExpiration = new Date(tokenFromJwt?.exp * 1000);
+//   const tokenFromJwt: any = jwtoken.decode(
+//     tokenEncapsulated?.accessToken.split(" ")[1]
+//   );
+//   const tokenExpiration = new Date(tokenFromJwt?.exp * 1000);
 
-  console.log("Checando middleware 🔑");
+//   console.log("Checando middleware 🔑");
 
-  // no token || expired
-  if (
-    !tokenEncapsulated ||
-    !tokenFromJwt ||
-    !request.cookies.has("next-auth.session-token") ||
-    new Date() > new Date(tokenExpiration)
-  ) {
-    request.cookies.clear();
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.rewrite(url);
-  }
+//   // no token || expired
+//   if (
+//     !tokenEncapsulated ||
+//     !tokenFromJwt ||
+//     !request.cookies.has("next-auth.session-token") ||
+//     new Date() > new Date(tokenExpiration)
+//   ) {
+//     request.cookies.clear();
+//     const url = request.nextUrl.clone();
+//     url.pathname = "/login";
+//     return NextResponse.redirect(url);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: [
